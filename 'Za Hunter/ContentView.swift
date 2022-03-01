f569e4d327a9f248166dfc86ec1c4f40fdf8c14f
Jack Louis <jbl5925@gmail.com>
2022-03-01 10:44:09 -0600

    Setup Initial Map

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.159332,
+            longitude: -88.138847),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
